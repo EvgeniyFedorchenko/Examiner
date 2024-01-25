a9d2ab67199fc54b4fb7be017c3f710fc6209a76
EvgeniyFedorchenko <jecky432@gmail.com>
2024-01-26 01:35:58 +0300

    add NotAllowedException

--- src/main/java/com/evgenyfedorchenko/examiner/exceptions/MethodNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.evgenyfedorchenko.examiner.exceptions;
-
-public class MethodNotAllowedException extends RuntimeException {
-    public MethodNotAllowedException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/evgenyfedorchenko/examiner/exceptions/NotAllowedException.java
@@ -0,0 +1,13 @@
+package com.evgenyfedorchenko.examiner.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+public class NotAllowedException extends RuntimeException {
+
+    public NotAllowedException(String message) {
+        super(message);
+    }
+
+}
